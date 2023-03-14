e1fa4bc43df7fd39c152e54b04fbabd7e1389683
ValentinaErpyleva <127898337+ValentinaErpyleva@users.noreply.github.com>
2023-03-15 00:28:26 +0300

    Homework
    Homework Soft Eng

---
+++ choose language.py
@@ -0,0 +1,9 @@
+print('Choose language Russian=1 Chineese=2 English=3')
+language=input ()
+if language=="1":
+    print('Privet'*5)
+elif language=='2':
+    print('Ni hao'*5)
+elif language=='3':
+    print('Hello'*5)
+
